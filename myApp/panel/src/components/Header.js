4664fdd65bce46fbec08b0716469e6de4fe3ed1e
malimz <46836766+malimz@users.noreply.github.com>
2019-04-16 22:44:24 +0430

    username modified

--- myApp/panel/src/components/Header.js
+++ myApp/panel/src/components/Header.js
@@ -15,21 +15,21 @@ class Header extends React.Component {
       const data = {}
       Axios.post('//localhost:3000/whoAmI', data).then(response => {
          if (response.data.success) {
-               this.setState({ userData: response.data.user });
+               this.setState({ userData: response.data.user[0] });
          } else { this.setState({ error: true });}
       })
    }
 
    
     render() {
-      const {userData} = this.state;
-      console.log(userData)
+//       const {userData} = this.state;
+//       console.log(userData)
        return (
           <div id="header">
              <div className= "fixed-header">
                 <div className="content-wrapper header-content">
                    <div className="app-title">
-                      <div className="title">Hi {userData.username}</div>
+                      <div className="title">Hi {this.state.userData['username']}</div>
                    </div>
                    <div className="header-right-side">
                       <div className="user-avatar">
